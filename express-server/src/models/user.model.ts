c460f5ee8a5a9b17188367e4895531b5415beafc
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-19 19:33:18 +0530

    Update user.model.ts

--- express-server/src/models/user.model.ts
+++ express-server/src/models/user.model.ts
@@ -5,7 +5,10 @@ import bcrypt from "bcrypt";
 export interface UserInput {
   firstName: string;
   lastName: string;
+  userName: string;
   email: string;
   password: string;
+  confirmPassword: string;
+  phone: string;
   gender: string;
 }
